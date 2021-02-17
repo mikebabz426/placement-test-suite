c44781e0d9dc4409f469ffcf9d3b09d63a8b807d
Mike Babetchki <mike.babetchki@gmail.com>
2021-02-17 17:39:02 -0600

    add dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
   </>
 )
 
