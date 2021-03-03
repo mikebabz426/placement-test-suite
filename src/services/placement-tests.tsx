8127b665f7fe5c094e5a099ae59f811517ba444f
Mike Babetchki <mike.babetchki@gmail.com>
2021-03-03 15:31:19 -0600

    add first half of placement test questions

--- src/services/placement-tests.tsx
+++ src/services/placement-tests.tsx
@@ -56,4 +56,244 @@ export const QuickPlacement = [
     ],
     answer: "a)  my sister's friend.",
   },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `${blank} 20 desks in the classroom.`,
+    options: ["a)  This is", "b)  There is", "c)  They are", "d)  There are"],
+    answer: "d)  There are",
+  },
+  {
+    question: `Paul ${blank} romantic films.`,
+    options: [
+      "a)  likes not",
+      "b)  don't like",
+      "c)  doesn't like",
+      "d)  isn't likes",
+    ],
+    answer: "c)  doesn't like",
+  },
+  {
+    question: `Sorry, I can't talk. I ${blank} right now.`,
+    options: ["a)  driving", "b)  'm driving'", "c)  drives", "d)  drive"],
+    answer: "b)  'm driving",
+  },
+  {
+    question: `She ${blank} at school last week.`,
+    options: ["a)  didn't be", "b)  weren't", "c)  wasn't", "d)  isn't"],
+    answer: "c)  wasn't",
+  },
+  {
+    question: `I ${blank} the film last night.`,
+    options: ["a)  like", "b)  likes", "c)  liking", "d)  liked"],
+    answer: "d)  liked",
+  },
+  {
+    question: `${blank} a piece of cake? No, thank you.`,
+    options: [
+      "a)  Do you like",
+      "b)  Would you like",
+      "c)  Want you",
+      "d)  Are you like",
+    ],
+    answer: "b)  Would you like",
+  },
+  {
+    question: `The living room is ${blank} than the bedroom.`,
+    options: ["a)  more big", "b)  more bigger", "c)  biggest", "d)  bigger"],
+    answer: "d)  bigger",
+  },
+  {
+    question: `The car is very old. We're going ${blank} a new car soon.`,
+    options: ["a)  to buy", "b)  buying", "c)  to will buy", "d)  buy"],
+    answer: "a)  to buy",
+  },
+  {
+    question: `Jane is a vegetarian. She ${blank} meat.`,
+    options: [
+      "a)  sometimes eats",
+      "b)  never eats",
+      "c)  often eats",
+      "d)  usually eats",
+    ],
+    answer: "b)  never eats",
+  },
+  {
+    question: `There aren't ${blank} buses late in the evening.`,
+    options: ["a)  some", "b)  any", "c)  no", "d)  a"],
+    answer: "b)  any",
+  },
+  {
+    question: `The car park is ${blank} to the restaurant.`,
+    options: ["a)  next", "b)  opposite", "c)  behind", "d)  in front"],
+    answer: "a)  next",
+  },
+  {
+    question: `Sue ${blank} shopping every day.`,
+    options: ["a)  is going", "b)  go", "c)  going", "d)  goes"],
+    answer: "d)  goes",
+  },
+  {
+    question: `They ${blank} in the park when it started to rain heavily.`,
+    options: [
+      "a)  walked",
+      "b)  were walking",
+      "c)  were walk",
+      "d)  are walking",
+    ],
+    answer: "b)  were walking",
+  },
+  {
+    question: `${blank} seen fireworks before?`,
+    options: [
+      "a)  Did you ever",
+      "b)  Are you ever",
+      "c)  Have you ever",
+      "d)  Do you ever",
+    ],
+    answer: "c)  Have you ever",
+  },
+  {
+    question: `We've been friends ${blank} many years.`,
+    options: ["a)  since", "b)  from", "c)  during", "d) for"],
+    answer: "d)  for",
+  },
+  {
+    question: `You ${blank} pay for the tickets. They're free.`,
+    options: [
+      "a)  have to",
+      "b)  don't have",
+      "c)  don't need to",
+      "d)  doesn't have to",
+    ],
+    answer: "c)  don't need to",
+  },
+  {
+    question: `Jeff was ill last week and he ${blank} go out.`,
+    options: ["a)  needn't", "b)  can't", "c)  mustn't", "d)  couldn't"],
+    answer: "d)  couldn't",
+  },
+  {
+    question: `These are the photos ${blank} I took on holiday.`,
+    options: ["a)  which", "b)  who", "c)  what", "d)  where"],
+    answer: "a)  which",
+  },
+  {
+    question: `We'll stay at home if it ${blank} this afternoon.`,
+    options: ["a)  raining", "b)  rains", "c)  will rain", "d)  rain"],
+    answer: "b)  rains",
+  },
+  {
+    question: `He doesn't smoke now, but he ${blank} a lot when he was young.`,
+    options: [
+      "a)  has smoked",
+      "b)  smokes",
+      "c)  used to smoke",
+      "d)  was smoked",
+    ],
+    answer: "c)  used to smoke",
+  },
+  {
+    question: `Mark plays football ${blank} anyone else I know.`,
+    options: [
+      "a)  more good than",
+      "b)  as better as",
+      "c)  best than",
+      "d)  better than",
+    ],
+    answer: "d)  better than",
+  },
+  {
+    question: `I promise I ${blank} you as soon as I've finished this cleaning.`,
+    options: [
+      "a)  will help",
+      "b)  am helping",
+      "c)  going to help",
+      "d)  have helped",
+    ],
+    answer: "a)  will help",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
+  {
+    question: `My brother is ${blank} artist.`,
+    options: ["a)  the", "b)  an", "c)  a", "d)  -"],
+    answer: "b)  an",
+  },
 ]
